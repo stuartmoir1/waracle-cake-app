f1c156e5e2b3273e5d7e36c56ebdc677281faf83
Stuart Moir <stuart.a.moir@live.com>
2020-10-18 13:25:20 +0100

    Added initial Layout component

---
+++ components/Layout.js
@@ -0,0 +1,17 @@
+export const siteTitle = 'Cakes'
+
+export const Layout = props => (
+  <div>
+    <div className='layout'>{props.children}</div>
+    <style jsx global>{`
+      body {
+        margin: 20px;
+        padding: 20px;
+        font-size: 16px;
+        background: rgba(252, 245, 199, 0.8);
+      }
+    `}</style>
+  </div>
+)
+
+export default Layout
